fcf741d8c67e172aa38df86dd67ea83f11054b88
UncleSem1 <zilcovs27@gmail.com>
2025-07-07 13:29:06 +0300

    On branch main
     Changes to be committed:
    	modified:   src/main/java/com/example/hhanalyzer/api/VacancyFetcher.java
    	modified:   target/classes/com/example/hhanalyzer/CharsetTest.class
    	modified:   target/classes/com/example/hhanalyzer/Main.class
    	modified:   target/classes/com/example/hhanalyzer/MiniServer.class
    	modified:   target/classes/com/example/hhanalyzer/api/ApiTest.class
    	modified:   target/classes/com/example/hhanalyzer/api/HhApiClient.class
    	modified:   target/classes/com/example/hhanalyzer/api/VacancyFetcher.class
    	modified:   target/classes/com/example/hhanalyzer/auth/AuthManager.class
    	modified:   target/classes/com/example/hhanalyzer/auth/OAuthAutoAuth.class
    	modified:   target/classes/com/example/hhanalyzer/db/DbService.class
    	modified:   target/classes/com/example/hhanalyzer/model/Employer.class
    	modified:   target/classes/com/example/hhanalyzer/model/Vacancy.class
    	modified:   target/classes/com/example/hhanalyzer/service/VacancyService.class
    	modified:   target/maven-status/maven-compiler-plugin/compile/default-compile/createdFiles.lst
    	modified:   target/maven-status/maven-compiler-plugin/compile/default-compile/inputFiles.lst

--- src/main/java/com/example/hhanalyzer/api/VacancyFetcher.java
+++ src/main/java/com/example/hhanalyzer/api/VacancyFetcher.java
@@ -1,13 +1,5 @@
 package com.example.hhanalyzer.api;
 
-
-import com.example.hhanalyzer.auth.AuthManager;
-
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.httpackage com.example.hhanalyzer;
-
 import com.example.hhanalyzer.auth.AuthManager;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -102,23 +94,4 @@ public class VacancyFetcher {
         }
     }
 }
-p.HttpResponse;
-
-public class ApiTest {
-    public static void main(String[] args) throws Exception {
-        AuthManager auth = new AuthManager();
-        String token = auth.getAccessToken();
-
-        HttpRequest request = HttpRequest.newBuilder()
-            .uri(URI.create("https://api.hh.ru/me"))
-            .header("Authorization", "Bearer " + token)
-            .build();
-
-        HttpClient client = HttpClient.newHttpClient();
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-        System.out.println("Ответ от hh.ru:");
-        System.out.println(response.body());
-    }
-}
-
 
